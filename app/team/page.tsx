071bb6ea3560442bb335e72ef99eee970f318e80
Pulkit Shukla <pulkitshukla001@gmail.com>
2025-03-30 23:17:42 +0530

    Added team page file

---
+++ app/team/page.tsx
@@ -0,0 +1,8 @@
+export default function TeamSection(){
+    return  (
+        <div className={`text-red-500 text-[35px]`}>
+            raj is stupid <br/>
+            pulkit is smart
+        </div>
+    )
+}
