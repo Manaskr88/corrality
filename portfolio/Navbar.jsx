1984ef6e33e8373bfe341cff274961c2dbf4ea73
Manas <manaspvt8@gmail.com>
2025-03-31 22:46:34 +0530

    checking

--- portfolio/Navbar.jsx
+++ portfolio/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>
-      Tech and Vibe
-    </div>
-  )
+    return (
+        <div>
+            <div className="">
+
+                <h2>Tech and Vibe</h2>
+                <div className="">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Team</li>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
